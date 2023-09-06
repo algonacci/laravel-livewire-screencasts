14200f11ba1782d19f3f5deac93f25647fa49f8b
Eric Julianto <algonacci1@gmail.com>
2023-09-06 22:23:03 +0700

    learn data binding

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,17 @@
 <div>
-    Hello {{ $name }}
+
+    <input type="text" wire:model.live.lazy="name">
+
+    <input type="checkbox" wire:model.live="loud">
+
+    <select wire:model.live="greeting" multiple>
+        <option>Hello</option>
+        <option>Good bye</option>
+        <option>Adios</option>
+    </select>
+
+
+    {{ implode(', ', $greeting) }} {{ strtoupper($name) }} @if ($loud)
+        !!!
+    @endif
 </div>
